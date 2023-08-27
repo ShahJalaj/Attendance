cd995f5d586d1980d18ea09a2dcf95422fd85bc0
Jalaj Shah <88316971+ShahJalaj@users.noreply.github.com>
2023-08-27 14:53:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './styles/App.css';
+import Header from './components/Header';
+import StudentList from './components/StudentList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <StudentList />
+    </div>
+  );
+}
+export default App;
